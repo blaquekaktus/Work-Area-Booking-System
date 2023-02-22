31b0a22b2e097bdb1cf08c701d17326649dff599
pabayr <pat.bayr@gmail.com>
2023-02-22 18:54:01 +0100

    RessourceRestController implementiert, Paar Exceptions hinzugefügt sowie addRessource Firebase implementiert

--- src/main/java/com/itkolleg/bookingsystem/repos/DBAccessRessource.java
+++ src/main/java/com/itkolleg/bookingsystem/repos/DBAccessRessource.java
@@ -2,13 +2,15 @@ package com.itkolleg.bookingsystem.repos;
 
 
 import com.itkolleg.bookingsystem.domains.Ressource;
+import com.itkolleg.bookingsystem.exceptions.RessourceExceptions.RessourceAlreadyExistsException;
 import com.itkolleg.bookingsystem.exceptions.RessourceExceptions.RessourceDeletionNotPossibleException;
 import com.itkolleg.bookingsystem.exceptions.RessourceExceptions.RessourceNotFoundException;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public interface DBAccessRessource {
-    public Ressource addRessource(Ressource ressource);
+    public Ressource addRessource(Ressource ressource) throws ExecutionException, InterruptedException, RessourceAlreadyExistsException;
     public List<Ressource> getAllRessource();
     public Ressource getRessourceById(Long id) throws RessourceNotFoundException;
     public Ressource updateRessourceById(Long id) throws RessourceNotFoundException;
