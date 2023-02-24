a6c216ec7a2a8802bf990c10ffa964bc89b4a772
pwnzZzZz <99125058+pwnzZzZz@users.noreply.github.com>
2023-02-24 12:52:56 +0100

    RessourceRESTController fixed!

--- src/main/java/com/itkolleg/bookingsystem/WABSRunner.java
+++ src/main/java/com/itkolleg/bookingsystem/WABSRunner.java
@@ -14,6 +14,7 @@ import java.util.Objects;
 
 @SpringBootApplication
 public class WABSRunner {
+    //
     public static void main(String[] args) throws IOException {
         // Erstelle einen ClassLoader für die WABSRunner-Klasse
         ClassLoader classLoader = WABSRunner.class.getClassLoader();
