0bb7aeb5ec11f516f06d454b6b3468eff9e5b865
pwnzZzZz <99125058+pwnzZzZz@users.noreply.github.com>
2023-05-01 14:36:11 +0200

    Login Controller und WebSecurityConfig Kommentare ergänzt!

--- src/main/java/com/itkolleg/bookingsystem/WebSecurityConfig.java
+++ src/main/java/com/itkolleg/bookingsystem/WebSecurityConfig.java
@@ -8,12 +8,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -25,48 +22,63 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-
     private final EmployeeService employeeService;
 
     public WebSecurityConfig(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
 
-
+    /**
+     Bean zum Verschlüsseln des Passworts mit bcrypt-Algorithmus.
+     @return das BCryptPasswordEncoder-Objekt.
+     */
     @Bean
     public BCryptPasswordEncoder bCryptpasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authConfig -> {
-                    authConfig.requestMatchers(HttpMethod.GET, "/*", "/web/login", "/web/hello", "/error", "/login-error", "/web/logout", "/css/**").permitAll();
-                    authConfig.requestMatchers(HttpMethod.POST, "/web/login").permitAll();
-                    authConfig.requestMatchers(HttpMethod.GET, "/web/allemployees").hasRole("USER");
-                    authConfig.requestMatchers(HttpMethod.GET, "/admin").hasRole("ADMIN");
-                    authConfig.requestMatchers(HttpMethod.GET, "/developer").hasRole("DEVELOPER");
-                    authConfig.requestMatchers(HttpMethod.GET, "/users").hasAnyRole("DEVELOPER");
-                    authConfig.requestMatchers(HttpMethod.GET, "/authorities").hasAnyRole("DEVELOPER");
-                    authConfig.anyRequest().authenticated();
-                })
-                .formLogin(login -> {
-                            login.loginPage("/web/login");
-                            login.defaultSuccessUrl("/web/hello");
-                            login.failureUrl("/login-error");
-                        }
-                )
-                .logout(logout -> {
-                    logout.logoutRequestMatcher(new AntPathRequestMatcher("/web/logout"));
-                    logout.logoutSuccessUrl("/web/login");
-                    logout.deleteCookies("JSESSIONID");
-                    logout.invalidateHttpSession(true);
-                });
-        return http.build();
-    }
+    /**
+     Konfiguriert die Sicherheitseinstellungen für die Http-Requests.
+     @param http Die HttpSecurity-Instanz.
+     @return die SecurityFilterChain-Instanz.
+     @throws Exception wenn Fehler bei der Konfiguration auftreten.
+     */
+     @Bean
+     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+     http
+     .authorizeHttpRequests(authConfig -> {
+     authConfig.requestMatchers(HttpMethod.GET, "/", "/web/login", "/web/hello", "/error", "/login-error", "/web/logout", "/css/**").permitAll();
+     authConfig.requestMatchers(HttpMethod.POST, "/web/login").permitAll();
+     authConfig.requestMatchers(HttpMethod.GET, "/web/allemployees").hasRole("USER");
+     authConfig.requestMatchers(HttpMethod.GET, "/admin").hasRole("ADMIN");
+     authConfig.requestMatchers(HttpMethod.GET, "/developer").hasRole("DEVELOPER");
+     authConfig.requestMatchers(HttpMethod.GET, "/users").hasAnyRole("DEVELOPER");
+     authConfig.requestMatchers(HttpMethod.GET, "/authorities").hasAnyRole("DEVELOPER");
+     authConfig.anyRequest().authenticated();
+     })
+     .formLogin(login -> {
+     login.loginPage("/web/login");
+     login.defaultSuccessUrl("/web/hello");
+     login.failureUrl("/login-error");
+     }
+     )
+     .logout(logout -> {
+     logout.logoutRequestMatcher(new AntPathRequestMatcher("/web/logout"));
+     logout.logoutSuccessUrl("/web/login");
+     logout.deleteCookies("JSESSIONID");
+     logout.invalidateHttpSession(true);
+     });
+     return http.build();
+     }
 
-
+     /**
+     Bean für den AuthenticationManager.
+     @param httpSecurity Die HttpSecurity-Instanz.
+     @param userDetailsService Das UserDetailsService-Objekt.
+     @param bCryptPasswordEncoder Das BCryptPasswordEncoder-Objekt.
+     @return die AuthenticationManager-Instanz.
+     @throws Exception wenn Fehler bei der Konfiguration auftreten.
+     */
     @Bean
     public AuthenticationManager authenticationManager(HttpSecurity httpSecurity, UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) throws Exception {
         AuthenticationManagerBuilder authenticationManagerBuilder = httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
@@ -75,7 +87,11 @@ public class WebSecurityConfig {
         return authenticationManagerBuilder.build();
     }
 
-
+    /**
+     Bean für den UserDetailsService.
+     @param bCryptPasswordEncoder Das BCryptPasswordEncoder-Objekt.
+     @return das UserDetailsService-Objekt.
+     */
     @Bean
     public UserDetailsService userDetailsService(BCryptPasswordEncoder bCryptPasswordEncoder) {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
@@ -89,7 +105,4 @@ public class WebSecurityConfig {
                 .build());
         return manager;
     }
-
-
 }
-
--- src/main/java/com/itkolleg/bookingsystem/controller/login/LoginWebController.java
+++ src/main/java/com/itkolleg/bookingsystem/controller/login/LoginWebController.java
@@ -1,8 +1,6 @@
 package com.itkolleg.bookingsystem.controller.login;
 
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -15,24 +13,41 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.security.Principal;
 
+/**
+ * Der LoginWebController ist verantwortlich für die Verarbeitung von HTTP-Anforderungen, die mit der Benutzeranmeldung
+ * und -authentifizierung im Zusammenhang stehen, und steuert den Ablauf des Anmelde- und Authentifizierungsprozesses.
+ */
 @Controller
 public class LoginWebController {
 
-
     private final AuthenticationManager authenticationManager;
 
+    /**
+     * Erzeugt eine neue Instanz von LoginWebController.
+     *
+     * @param authenticationManager ein AuthenticationManager-Objekt zur Authentifizierung des Benutzers.
+     */
     public LoginWebController(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
     }
 
-
+    /**
+     * Zeigt das Login-Formular an.
+     *
+     * @return der Name des HTML-Formulars, das das Login-Formular darstellt.
+     */
     @GetMapping("/web/login")
     public String showLoginForm() {
         return "login/login";
     }
 
-
-
+    /**
+     * Zeigt die Begrüßungsseite nach einer erfolgreichen Anmeldung an.
+     *
+     * @param model     das Model-Objekt zur Verarbeitung und Anzeige von Daten in der Benutzeroberfläche.
+     * @param principal ein Principal-Objekt, das den angemeldeten Benutzer darstellt.
+     * @return der Name des HTML-Dokuments, das die Begrüßungsseite darstellt.
+     */
     @GetMapping("/web/hello")
     public String hello(Model model, Principal principal) {
         String username = principal.getName();
@@ -40,14 +55,18 @@ public class LoginWebController {
         return "/login/hello";
     }
 
+    /**
+     * Verarbeitet das Login-Formular und authentifiziert den Benutzer.
+     *
+     * @param request das HTTP-Request-Objekt, das die Benutzeranmeldedaten enthält.
+     * @return eine Weiterleitung auf die Begrüßungsseite, falls die Authentifizierung erfolgreich war, oder auf
+     * die Login-Fehlerseite, falls die Authentifizierung fehlgeschlagen ist.
+     */
     @PostMapping("/web/login")
-    public String processLoginForm(HttpServletRequest request, HttpServletResponse response) {
-
+    public String processLoginForm(HttpServletRequest request) {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
-
         try {
             Authentication authentication = authenticationManager.authenticate(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
@@ -57,6 +76,11 @@ public class LoginWebController {
         }
     }
 
+    /**
+     * Zeigt die Login-Fehlerseite an.
+     *
+     * @return der Name des HTML-Dokuments, das die Login-Fehlerseite darstellt.
+     */
     @GetMapping("/login-error")
     public String loginError() {
         return "login/login-error";
