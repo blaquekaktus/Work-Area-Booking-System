ad051c254a652fad20df64f17710e56bfea1089e
blaquekaktus <blaquekaktus@gmail.com>
2023-06-02 17:24:05 +0200

    Desks Javadoc Added

--- src/main/java/com/itkolleg/bookingsystem/domains/Desk.java
+++ src/main/java/com/itkolleg/bookingsystem/domains/Desk.java
@@ -6,44 +6,79 @@ import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
 import lombok.ToString;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class represents a Desk in the system.
+ * Each desk has an identifier, a number of monitors, and a list of ports.
+ */
 @Entity
 @Getter
 @Setter
 @ToString
-
 public class Desk {
-    @NotEmpty
-    @Size(min = 1)
+
+    private static final Logger logger = LoggerFactory.getLogger(Desk.class);
+
+    /**
+     * A list of ports available at the desk.
+     */
+    @NotEmpty(message = "Ports must not be empty")
+    @Size(min = 1, message = "At least one port is required")
     @ElementCollection
     protected List<Port> ports;
+
+    /**
+     * The identifier for the desk.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
-    @NotEmpty
+
+    /**
+     * The desk number.
+     */
+    @NotEmpty(message = "Desk number must not be empty")
     private String deskNr;
-    @Digits(integer = 2, fraction = 0)
-    @Min(0)
-    @Max(10)
+
+    /**
+     * The number of monitors at the desk.
+     */
+    @Digits(integer = 2, fraction = 0, message = "Number of monitors must be a positive integer (0 - 99)")
+    @Min(value = 0, message = "Number of monitors can't be less than 0")
+    @Max(value = 10, message = "Number of monitors can't exceed 10")
     private int nrOfMonitors;
 
+    /**
+     * Default constructor that initializes the list of ports.
+     */
     public Desk() {
         this.ports = new ArrayList<>();
     }
 
+    /**
+     * Constructor for creating a Desk with a specific desk number, number of monitors, and a list of ports.
+     */
     public Desk(@NonNull String deskNr, int nrOfMonitors, List<Port> ports) {
         this.deskNr = deskNr;
         this.nrOfMonitors = nrOfMonitors;
         this.ports = ports;
     }
 
+    /**
+     * Method to add a port to the list of ports for the desk.
+     */
     public void addPort(Port port) {
         if (ports != null) this.ports.add(port);
     }
 
+    /**
+     * Method to delete all ports from the list of ports for the desk.
+     */
     public void deleteAllPorts() {
         this.ports.clear();
     }
