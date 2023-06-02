23a876288d75eda25d8097b20260a91671512de3
blaquekaktus <blaquekaktus@gmail.com>
2023-06-02 17:28:59 +0200

    ErrorDetails javadoc added

---
+++ src/main/java/com/itkolleg/bookingsystem/exceptions/BookingExceptions/DeskBookingDeletionFailureException.java
@@ -0,0 +1,7 @@
+package com.itkolleg.bookingsystem.exceptions.BookingExceptions;
+
+public class DeskBookingDeletionFailureException extends Throwable {
+    public DeskBookingDeletionFailureException(String message) {
+        super(message);
+    }
+}
