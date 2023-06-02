4c2957d8566693b2d6c1fe018572657e5c1752d3
blaquekaktus <blaquekaktus@gmail.com>
2023-06-02 17:17:55 +0200

    Javadoc created for DeskBooking Class

--- src/main/java/com/itkolleg/bookingsystem/domains/Booking/DeskBooking.java
+++ src/main/java/com/itkolleg/bookingsystem/domains/Booking/DeskBooking.java
@@ -7,12 +7,19 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+/**
+ * This class extends the Booking class to include a Desk.
+ * It represents a desk booking within the system.
+ */
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
@@ -21,10 +28,20 @@ import java.time.LocalTime;
 @ToString(callSuper = true)
 public class DeskBooking extends Booking {
 
+    private static final Logger logger = LoggerFactory.getLogger(DeskBooking.class);
+
+    /**
+     * The desk associated with the booking.
+     * It must not be null.
+     */
+    @NotNull(message = "Desk must not be null")
     @ToString.Include
     @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
     private Desk desk;
 
+    /**
+     * Constructor for creating a desk booking with specific employee, desk, date, start and end times, and timestamp.
+     */
     public DeskBooking(Employee employee, Desk desk, LocalDate date, LocalTime start, LocalTime endTime, LocalDateTime timeStamp) {
         super(employee, date, start, endTime, timeStamp);
         this.desk = desk;
