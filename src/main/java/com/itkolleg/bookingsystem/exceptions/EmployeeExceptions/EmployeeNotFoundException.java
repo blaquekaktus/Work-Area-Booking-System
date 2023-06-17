343b148b13d542fc4175c675e07a8a16df8c3dc4
blaquekaktus <blaquekaktus@gmail.com>
2023-06-17 18:26:11 +0200

    Resolved merge conflicts

--- src/main/java/com/itkolleg/bookingsystem/exceptions/EmployeeExceptions/EmployeeNotFoundException.java
+++ src/main/java/com/itkolleg/bookingsystem/exceptions/EmployeeExceptions/EmployeeNotFoundException.java
@@ -3,7 +3,6 @@ package com.itkolleg.bookingsystem.exceptions.EmployeeExceptions;
 public class EmployeeNotFoundException extends Exception {
 
     public EmployeeNotFoundException(String s){
-
         super("Mitarbeiter nicht gefunden!");
     }
 }
