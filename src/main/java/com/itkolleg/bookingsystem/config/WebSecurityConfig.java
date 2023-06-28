0b529fe443bef4ad22bb8e334e180a28a003261f
Manuel Payer <manuel.payer96@gmx.at>
2023-06-28 18:02:40 +0200

    Problemlösung

--- src/main/java/com/itkolleg/bookingsystem/config/WebSecurityConfig.java
+++ src/main/java/com/itkolleg/bookingsystem/config/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.itkolleg.bookingsystem.config;
 
-import com.itkolleg.bookingsystem.domains.CustomEmployeeDetails;
 import com.itkolleg.bookingsystem.domains.TimeSlot;
 import com.itkolleg.bookingsystem.service.Desk.DeskService;
 import com.itkolleg.bookingsystem.service.DeskBooking.DeskBookingService;
@@ -12,13 +11,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -26,8 +22,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 
 /**
@@ -79,43 +73,51 @@ public class WebSecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-
-                .headers()
-                .contentTypeOptions()
-                .disable()
-                .and()
+                /* .headers()
+                 .contentTypeOptions()
+                 .disable()
+                 .and() */
                 .authorizeHttpRequests(authConfig -> {
-                    authConfig.requestMatchers(HttpMethod.GET, "/web/login", "/error", "/web/login-error", "/web/logout", "/static/**", "/templates/**").permitAll();
-                    authConfig.requestMatchers(HttpMethod.GET,  "/web/employees/start", "/web/ressource/**", "/web/roomBooking/**", "/web/ressourceBooking/**", "/web/floorsEmployee","/web/desks/**","/web/room/**").hasAnyRole("OPERATOR", "N_EMPLOYEE", "P_EMPLOYEE");
+                    authConfig.requestMatchers(HttpMethod.GET, "/web/**", "/web/login", "/error", "/web/login-error", "/web/logout", "/static/**", "/templates/**").permitAll();
+                    authConfig.requestMatchers(HttpMethod.POST, "/web/login", "web/**").permitAll();
                     authConfig.requestMatchers(HttpMethod.GET, "/web/**").hasRole("ADMIN");
-                    authConfig.requestMatchers(HttpMethod.POST, "/web/login").permitAll();
-                    authConfig.requestMatchers(HttpMethod.POST, "/web/desks/**", "/web/ressource/**", "/web/roomBooking/**", "/web/ressourceBooking/**", "/web/room").hasAnyRole("OPERATOR", "N_EMPLOYEE", "P_EMPLOYEE");
+                    authConfig.requestMatchers(HttpMethod.GET,  "/web/**").hasAnyRole("OPERATOR", "N_EMPLOYEE", "P_EMPLOYEE");
                     authConfig.requestMatchers(HttpMethod.POST, "/web/**").hasRole("ADMIN");
+                    authConfig.requestMatchers(HttpMethod.POST, "/web/**").hasAnyRole("OPERATOR", "N_EMPLOYEE", "P_EMPLOYEE");
+                    /*authConfig.requestMatchers(HttpMethod.GET, "/web/hello", "/web/**","/web/login", "/error", "/web/login-error", "/web/logout", "/static/**", "/templates/**").permitAll();
+                    authConfig.requestMatchers(HttpMethod.POST, "/web/**", "/web/login").permitAll();
+                    authConfig.requestMatchers(HttpMethod.GET, "/web/allemployees", "/web/insertemployeeform", "/web/insertemployee", "/web/admin-start", "/web/editemployee/**", "/web/deleteemployee/**").hasRole("ADMIN");
+                    authConfig.requestMatchers(HttpMethod.GET, "/web/deskbookings/admin/**","/web/desks/**","/web/admin-start" ).hasAnyRole("ADMIN", "OPERATOR");
+                    authConfig.requestMatchers(HttpMethod.POST, "/web/deskbookings/admin/**","/web/desks/**", "/web/admin-start" ).hasAnyRole("ADMIN", "OPERATOR");
+                    */
+                    /*authConfig.requestMatchers(HttpMethod.GET, "/web").hasRole("ADMIN");
+                    authConfig.requestMatchers(HttpMethod.GET, "/operator").hasRole("OPERATOR");
+                    authConfig.requestMatchers(HttpMethod.GET, "/users").hasAnyRole("Admin", "DEVELOPER");
+                    authConfig.requestMatchers(HttpMethod.GET, "/authorities").hasAnyRole("DEVELOPER");
+                    authConfig.anyRequest().authenticated();*/
                 })
                 .formLogin(login -> {
                     login.loginPage("/web/login")
-                            .failureHandler((request, response, exception) -> {
-                                String errorMessage = "Invalid username or password.";
-                                response.sendRedirect("/web/login-error" + errorMessage);
-                            })
                             .successHandler((request, response, authentication) -> {
                                 Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
                                 if (roles.contains("ROLE_ADMIN")) {
-                                    response.sendRedirect("/web/employees/admin-start");
+                                    response.sendRedirect("/web/admin-start");
                                 } else if (roles.contains("ROLE_OPERATOR")) {
-                                    response.sendRedirect("/web/employees/start");
+                                    response.sendRedirect("/web/hello");
                                 } else if (roles.contains("ROLE_N_EMPLOYEE")) {
-                                    response.sendRedirect("/web/employees/start");
+                                    response.sendRedirect("/web/hello");
                                 } else if (roles.contains("ROLE_P_EMPLOYEE")) {
-                                    response.sendRedirect("/web/employees/start");
+                                    response.sendRedirect("/web/hello");
                                 } else {
                                     response.sendRedirect("/web/login-error");
                                 }
-                            });
+                            })
+                            .failureUrl("/web/login-error");
                 })
+
                 .logout(logout -> {
                     logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
-                    logout.logoutSuccessUrl("/web/login");
+                    logout.logoutSuccessUrl("/web/logout");
                     logout.deleteCookies("JSESSIONID");
                     logout.invalidateHttpSession(true);
                 });
@@ -139,6 +141,7 @@ public class WebSecurityConfig {
         return authenticationManagerBuilder.build();
     }
 
+
     /**
      * Bean für den UserDetailsService.
      *
@@ -159,4 +162,9 @@ public class WebSecurityConfig {
                     .build();
         };
     }
+
+
+
+
+
 }
