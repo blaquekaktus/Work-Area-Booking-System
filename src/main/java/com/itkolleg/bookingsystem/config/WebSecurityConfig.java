2613c153d5496ad46c69a4435514e227977b83a4
pabayr <pat.bayr@gmail.com>
2023-06-29 12:09:15 +0200

    for the boys

--- src/main/java/com/itkolleg/bookingsystem/config/WebSecurityConfig.java
+++ src/main/java/com/itkolleg/bookingsystem/config/WebSecurityConfig.java
@@ -79,10 +79,10 @@ public class WebSecurityConfig {
                 .and()*/
                 .authorizeHttpRequests(authConfig -> {
                     authConfig.requestMatchers(HttpMethod.GET, "/web/login", "/error", "/web/login-error", "/web/logout", "/static/**", "/templates/**").permitAll();
-                    authConfig.requestMatchers(HttpMethod.GET, "/web/user/start", "/web/ressourceBooking/allBookingsEmployee", "/web/ressourceBooking/createBookingEmployee/**", "/web/ressourceBooking/deleteBookingEmployee/**","/web/deskbookings/mydeskbookings","/web/ressource/allRessourcesEmployee", "/web/room/allroomsEmployee", "/web/roomBooking/allBookingsEmployee").hasAnyRole("N_EMPLOYEE", "P_EMPLOYEE");
+                    authConfig.requestMatchers(HttpMethod.GET, "/web/user/start", "/web/ressourceBooking/allBookingsEmployee", "/web/ressourceBooking/createBookingEmployee/**", "/web/ressourceBooking/deleteBookingEmployee/**","/web/deskbookings/mydeskbookings","/web/ressource/allRessourcesEmployee", "/web/rooms/allRoomsEmployee", "/web/roomBooking/allBookingsEmployee","/web/roomBooking/createBookingEmployee/**").hasAnyRole("N_EMPLOYEE", "P_EMPLOYEE");
                     authConfig.requestMatchers(HttpMethod.GET, "/web/**").hasAnyRole("ADMIN", "OPERATOR");
                     authConfig.requestMatchers(HttpMethod.POST, "/web/login").permitAll();
-                    authConfig.requestMatchers(HttpMethod.POST, "/web/desks/**", "/web/ressource/**", "/web/roomBooking/**", "/web/ressourceBooking/**", "/web/room/**").hasAnyRole( "N_EMPLOYEE", "P_EMPLOYEE");
+                    authConfig.requestMatchers(HttpMethod.POST, "/web/desks/**", "/web/ressource/**", "/web/roomBooking/**", "/web/ressourceBooking/**", "/web/rooms/**").hasAnyRole( "N_EMPLOYEE", "P_EMPLOYEE");
                     authConfig.requestMatchers(HttpMethod.POST, "/web/**").hasAnyRole("ADMIN", "OPERATOR");
 
                 })
