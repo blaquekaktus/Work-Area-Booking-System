8a7d8b9d7bc5ece074d46bf847df904c64f18bf2
@blaquekaktus <blaqukaktus@gmail.com>
2023-09-18 10:47:44 +0200

    Global Exception Handler

--- src/main/java/com/itkolleg/bookingsystem/controller/GlobalExceptionController.java
+++ src/main/java/com/itkolleg/bookingsystem/controller/GlobalExceptionController.java
@@ -1,16 +1,13 @@
 package com.itkolleg.bookingsystem.controller;
 
 import com.itkolleg.bookingsystem.domains.ErrorCode;
-import com.itkolleg.bookingsystem.exceptions.ResourceNotFoundException;
-import com.itkolleg.bookingsystem.exceptions.DeskNotAvailableException;
-import com.itkolleg.bookingsystem.exceptions.EmployeeExceptions.EmployeeAlreadyExistsException;
-import com.itkolleg.bookingsystem.exceptions.EmployeeExceptions.EmployeeDeletionNotPossibleException;
-import com.itkolleg.bookingsystem.exceptions.EmployeeExceptions.EmployeeNotFoundException;
-import com.itkolleg.bookingsystem.exceptions.EmployeeExceptions.EmployeeValidationException;
-import com.itkolleg.bookingsystem.exceptions.ExceptionDTO;
-import com.itkolleg.bookingsystem.exceptions.FormValidationExceptionDTO;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.itkolleg.bookingsystem.exceptions.*;
+import com.itkolleg.bookingsystem.exceptions.employeeExceptions.EmployeeAlreadyExistsException;
+import com.itkolleg.bookingsystem.exceptions.employeeExceptions.EmployeeDeletionNotPossibleException;
+import com.itkolleg.bookingsystem.exceptions.employeeExceptions.EmployeeNotFoundException;
+import com.itkolleg.bookingsystem.exceptions.employeeExceptions.EmployeeValidationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,97 +16,88 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import static com.itkolleg.bookingsystem.domains.ViewConstants.*;
 
 /**
  *
  * This class handles global exceptions for the booking system.
  * @author Sonja Lechner, Marcel Schranz
- * @version 1.0
- * @since 2023-05-24
+ * @version 1.3
+ * @since 2023-09-03
  */
 
+@Slf4j
+@Order(1)
 @ControllerAdvice
 public class GlobalExceptionController {
-    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionController.class);
 
-
-    @ExceptionHandler(EmployeeNotFoundException.class)
-    public ResponseEntity<ExceptionDTO> employeeNotFound(EmployeeNotFoundException employeeNotFoundException) {
-        return new ResponseEntity<>(new ExceptionDTO("1000", employeeNotFoundException.getMessage()), HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(EmployeeValidationException.class)
-    public ResponseEntity<FormValidationExceptionDTO> employeeValidationException(EmployeeValidationException employeeValidationException) {
-        return new ResponseEntity<>(employeeValidationException.getErrorMap(), HttpStatus.NOT_ACCEPTABLE);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ExceptionDTO> employeeDeletionNotPossibleException(EmployeeDeletionNotPossibleException employeeDeletionNotPossibleException) {
-        return new ResponseEntity<>(new ExceptionDTO("1000", employeeDeletionNotPossibleException.getMessage()), HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(EmployeeAlreadyExistsException.class)
-    public ResponseEntity<ExceptionDTO> employeeAlreadyExists(EmployeeAlreadyExistsException employeeAlreadyExistsException) {
-        return new ResponseEntity<>(new ExceptionDTO("1000", employeeAlreadyExistsException.getMessage()), HttpStatus.NOT_FOUND);
+    /**
+     * Handles custom illegal argument exceptions.
+     *
+     * @param
+     * @param redirectAttributes The RedirectAttributes object to add flash attributes.
+     * @return A string representing the redirect path.
+     */
+    @ExceptionHandler(CustomIllegalArgumentException.class)
+    public String handleCustomIllegalArgumentException(CustomIllegalArgumentException ex, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("errorMessage", ex.getMessage());
+        return switch (ex.getOrigin()) {
+            case "AdminAddDeskBooking" -> "redirect:" + A_ADD_DESKBOOKING;
+            case "AdminNewDeskBooking" -> "redirect:" + A_NEW_DESKBOOKING;
+            case "AdminUpdateDeskBooking" -> "redirect:" + A_UPDATE_DESKBOOKING;
+            case "EmpAddDeskBooking" -> "redirect:" + E_ADD_DESKBOOKING;
+            case "EmpNewDeskBooking" -> "redirect:" + E_NEW_DESKBOOKING;
+            case "EmpUpdateDeskBooking" -> "redirect:" + E_UPDATE_DESKBOOKING;
+            default -> "redirect:/error";
+        };
     }
 
     /**
-     * Handles the ResourceNotFoundException and redirects to the error page.
-     *
-     * @param e                 The ResourceNotFoundException instance.
-     * @param redirectAttributes The RedirectAttributes object to add flash attributes.
-     * @return A string representing the redirect path to the error page.
+     * Centralizes the logic for handling exceptions and redirecting to the error page.
      */
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public String handleResourceNotFound(ResourceNotFoundException e, RedirectAttributes redirectAttributes) {
-        logger.error("ResourceNotFoundException: {}", e.getMessage(), e);
-        redirectAttributes.addFlashAttribute("errorCode", ErrorCode.RESOURCE_NOT_FOUND.getCode());
-        redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+    private String handleExceptionAndRedirect(Exception e, ErrorCode errorCode, RedirectAttributes redirectAttributes) {
+        return handleExceptionAndRedirect(e, errorCode, redirectAttributes, e.getMessage());
+    }
+
+    /**
+     * Centralizes the logic for handling exceptions, setting error messages, and redirecting to the error page.
+     */
+    private String handleExceptionAndRedirect(Exception e, ErrorCode errorCode, RedirectAttributes redirectAttributes, String errorMessage) {
+        log.error("{}: {}", e.getClass().getSimpleName(), e.getMessage(), e);
+        redirectAttributes.addFlashAttribute("errorCode", errorCode.getCode());
+        redirectAttributes.addFlashAttribute("errorMessage", errorMessage);
         return "redirect:/error";
     }
 
     /**
-     * Handles the DeskNotAvailableException and redirects to the error page.
-     *
-     * @param e                  The DeskNotAvailableException instance.
-     * @param redirectAttributes The RedirectAttributes object to add flash attributes.
-     * @return A string representing the redirect path to the error page.
+     * Map of exceptions to their error codes.
      */
-    @ExceptionHandler(DeskNotAvailableException.class)
-    public String handleDeskNotAvailable(DeskNotAvailableException e, RedirectAttributes redirectAttributes) {
-        logger.error("DeskNotAvailableException: {}", e.getMessage(), e);
-        redirectAttributes.addFlashAttribute("errorCode", ErrorCode.DESK_NOT_AVAILABLE.getCode());
-        redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
-        return "redirect:/error";
+    private static final Map<Class<? extends Exception>, ErrorCode> ERROR_CODE_MAP = Map.of(
+            ResourceNotFoundException.class, ErrorCode.RESOURCE_NOT_FOUND,
+            ResourceDeletionFailureException.class, ErrorCode.RESOURCE_DELETION_FAILURE,
+            DeskNotAvailableException.class, ErrorCode.DESK_NOT_AVAILABLE,
+            EmptyResultDataAccessException.class, ErrorCode.DATA_ACCESS_ERROR,
+            MethodArgumentNotValidException.class, ErrorCode.METHOD_ARGUMENT_NOT_VALID
+    );
+
+    /**
+     * Handles exceptions that require a redirect to the error page.
+     */
+    @ExceptionHandler({ResourceNotFoundException.class, ResourceDeletionFailureException.class,
+            DeskNotAvailableException.class, EmptyResultDataAccessException.class, IllegalArgumentException.class})
+    public String handleRedirectExceptions(Exception e, RedirectAttributes redirectAttributes) {
+        return handleExceptionAndRedirect(e, ERROR_CODE_MAP.get(e.getClass()), redirectAttributes);
     }
 
     /**
-     * Handles the EmptyResultDataAccessException and redirects to the error page.
-     *
-     * @param e                  The EmptyResultDataAccessException instance.
-     * @param redirectAttributes The RedirectAttributes object to add flash attributes.
-     * @return A string representing the redirect path to the error page.
-     */
-    @ExceptionHandler(EmptyResultDataAccessException.class)
-    public String handleEmptyResultDataAccess(EmptyResultDataAccessException e, RedirectAttributes redirectAttributes) {
-        logger.error("EmptyResultDataAccessException: {}", e.getMessage(), e);
-        redirectAttributes.addFlashAttribute("errorCode", ErrorCode.DATA_ACCESS_ERROR.getCode());
-        redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
-        return "redirect:/error";
-    }
-
-    /**
-     * Handles the MethodArgumentNotValidException and redirects to the error page.
-     *
-     * @param ex                 The MethodArgumentNotValidException instance.
-     * @param redirectAttributes The RedirectAttributes object to add flash attributes.
-     * @return A string representing the redirect path to the error page.
+     * Handles validation exceptions and redirects to the error page.
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public String handleMethodArgumentNotValid(MethodArgumentNotValidException ex, RedirectAttributes redirectAttributes) {
-        logger.error("MethodArgumentNotValidException: {}", ex.getMessage(), ex);
-        String errorMessage = ex.getBindingResult().getFieldErrors().stream()
+    public String handleMethodArgumentNotValid(MethodArgumentNotValidException e, RedirectAttributes redirectAttributes) {
+        String errorMessage = e.getBindingResult().getFieldErrors().stream()
                 .map(fieldError -> fieldError.getField() + " " + fieldError.getDefaultMessage())
                 .collect(Collectors.joining(", "));
         redirectAttributes.addFlashAttribute("errorCode", ErrorCode.METHOD_ARGUMENT_NOT_VALID.getCode());
@@ -118,17 +106,42 @@ public class GlobalExceptionController {
     }
 
     /**
-     * Handles any general Exception and redirects to the error page.
-     *
-     * @param e                  The Exception instance.
-     * @param redirectAttributes The RedirectAttributes object to add flash attributes.
-     * @return A string representing the redirect path to the error page.
+     * Handles general exceptions and redirects to the error page.
      */
     @ExceptionHandler(Exception.class)
     public String handleGeneralException(Exception e, RedirectAttributes redirectAttributes) {
-        logger.error("Exception: {}", e.getMessage(), e);
-        redirectAttributes.addFlashAttribute("errorCode", ErrorCode.GENERAL_ERROR.getCode());
-        redirectAttributes.addFlashAttribute("errorMessage", "An unexpected error occurred.");
-        return "redirect:/error";
+        return handleExceptionAndRedirect(e, ErrorCode.GENERAL_ERROR, redirectAttributes, "An unexpected error occurred.");
+    }
+
+    /**
+     * Handles employee not found exceptions.
+     */
+    @ExceptionHandler(EmployeeNotFoundException.class)
+    public ResponseEntity<ExceptionDTO> employeeNotFound(EmployeeNotFoundException employeeNotFoundException) {
+        return new ResponseEntity<>(new ExceptionDTO("1000", employeeNotFoundException.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * Handles employee validation exceptions.
+     */
+    @ExceptionHandler(EmployeeValidationException.class)
+    public ResponseEntity<FormValidationExceptionDTO> employeeValidationException(EmployeeValidationException employeeValidationException) {
+        return new ResponseEntity<>(employeeValidationException.getErrorMap(), HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    /**
+     * Handles employee deletion not possible exceptions.
+     */
+    @ExceptionHandler
+    public ResponseEntity<ExceptionDTO> employeeDeletionNotPossibleException(EmployeeDeletionNotPossibleException employeeDeletionNotPossibleException) {
+        return new ResponseEntity<>(new ExceptionDTO("1000", employeeDeletionNotPossibleException.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * Handles employee already exists exceptions.
+     */
+    @ExceptionHandler(EmployeeAlreadyExistsException.class)
+    public ResponseEntity<ExceptionDTO> employeeAlreadyExists(EmployeeAlreadyExistsException employeeAlreadyExistsException) {
+        return new ResponseEntity<>(new ExceptionDTO("1000", employeeAlreadyExistsException.getMessage()), HttpStatus.NOT_FOUND);
     }
 }
